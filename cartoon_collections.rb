ca5f2006eb089b81fc2fcbe478124f443baa4a9e
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-06-15 15:52:19 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each_with_index{|dwarf,index| puts "#{index + 1} #{dwarf} "}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  cap_plan = array.collect{|call| "#{call.capitalize}!"}
+  cap_plan
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  bool = array.include?(array.find{|call| call.size >= 5})
+  return bool
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  count = 0
+ 3.times do
+  bool = array.include?(cheese_types[count])
+  if bool
+    return cheese_types[count]
+  end
+  count += 1
+end
+return nil
 end
